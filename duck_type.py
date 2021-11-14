85e1b88f24f23118d735303b110580e3bbbc8170
Barne Kleinen <barne.kleinen@htw-berlin.de>
2021-11-14 23:30:03 +0100

    improve duck type

--- duck_type.py
+++ duck_type.py
@@ -7,23 +7,23 @@ class Dog:
     def bark(self):
 	    return "woooof!"
 
-def func(maybe_a_duck):
+def function_that_expects_duck_type(maybe_a_duck):
 	return "let it quack: " + maybe_a_duck.quack()
 
 class TestDuckTyping(unittest.TestCase):
 
     def test_ducks_quack(self):
         duck = Duck()
-        self.assertEqual("let it quack: quaaack!",func(duck))
+        self.assertEqual("let it quack: quaaack!",function_that_expects_duck_type(duck))
 
     def test_dogs_dont_quack(self):
         dog = Dog()
-        with self.assertRaises(AttributeError): func(dog)
+        with self.assertRaises(AttributeError): function_that_expects_duck_type(dog)
 
     def test_make_this_dog_quack(self):
         dog = Dog()
         dog.quack = dog.bark
-        self.assertEqual("let it quack: woooof!",func(dog))
+        self.assertEqual("let it quack: woooof!",function_that_expects_duck_type(dog))
 
 if __name__ == '__main__':
     unittest.main()
