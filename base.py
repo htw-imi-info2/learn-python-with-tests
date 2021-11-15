94ab96ac1f6f59e4f5a35ac5332cbf6689129d79
Barne Kleinen <barne.kleinen@htw-berlin.de>
2021-11-15 17:17:54 +0100

    add exception examples

--- base.py
+++ base.py
@@ -1,10 +1,22 @@
 import unittest
 
+def broken_function():
+    raise Exception('This is broken')
+
 class TestSomething(unittest.TestCase):
 
     def test_a_string_variable(self):
         a = "Hello,  world!"
-        self.assertEqual("Hello,  world!",a)
+        self.assertEqual("Hello,  world!", a)
+
+    def test_expect_an_exception(self):
+        with self.assertRaises(Exception):
+            broken_function()
+
+    def test_undefined_as_example(self):
+        with self.assertRaises(NameError):
+            a = notdefined
+
 
 if __name__ == '__main__':
     unittest.main()
